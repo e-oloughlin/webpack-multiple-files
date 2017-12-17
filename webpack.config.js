732420e6459c68c8d4d46456ee59987a2581ec78
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-12-17 17:10:55 +0000

    Initialized repo

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    entry: {
+        module1: 'module1/main.js',
+        module2: 'module2/main.js',
+        module3: 'module3/main.js'
+    },
+    output: {
+        
+    }
+};
