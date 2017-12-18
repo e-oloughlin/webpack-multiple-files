2cd1cf38bc683f1a7ae0320b7a7f591533679906
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-12-18 07:54:48 +0000

    Bundling multiple files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,20 @@
+const path = require('path');
+const fs = require('fs');
+const readDirRecursive = require('recursive-readdir-synchronous');
+
+let files = readDirRecursive('./module', [(file, stats) => {
+    // Exclude files here....
+}]);
+
+console.log(files);
+
 module.exports = {
     entry: {
-        module1: 'module1/main.js',
-        module2: 'module2/main.js',
-        module3: 'module3/main.js'
+        'module/module1/module1': path.resolve(__dirname, 'module/module1/main.js'),
+        'module/module2/module2': path.resolve(__dirname, 'module/module2/main.js'),
+        'module/module3/module3': path.resolve(__dirname, 'module/module3/main.js')
     },
     output: {
-        
+        filename: '[name].bundle.js'
     }
 };
