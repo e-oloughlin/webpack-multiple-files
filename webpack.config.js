d4eec28a49cf1c43c4109cb5346033a8a33755db
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-12-30 15:02:51 +0000

    Dynamically generating entry files with outputs in the same directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,46 @@
 const path = require('path');
 const fs = require('fs');
+const _ = require('lodash');
 const readDirRecursive = require('recursive-readdir-synchronous');
 
-let files = readDirRecursive('./module', [(file, stats) => {
-    // Exclude files here....
-}]);
+/**
+ * Returns a webpack bundle name for filePath
+ * It's a bit of a hack. For a path like: '...module/module1/module1/main.js'
+ * it will return 'module/module1/module1'
+ * @param  {String} filePath    The file path for an entry path
+ * @return {String}
+ */
+const getBundleName = filePath => {
+    let parts = filePath.split('/'),
+        dir = parts[parts.length - 3],
+        moduleName = parts[parts.length - 2];
 
-console.log(files);
+    return [dir, moduleName, moduleName].join('/');
+}
 
-module.exports = {
-    entry: {
-        'module/module1/module1': path.resolve(__dirname, 'module/module1/main.js'),
-        'module/module2/module2': path.resolve(__dirname, 'module/module2/main.js'),
-        'module/module3/module3': path.resolve(__dirname, 'module/module3/main.js')
-    },
+/**
+ * Returns an array of js files for webpack to process
+ * @param  {String} basePath    The top level directory containing js modules
+ * @return {Array}              An array of javascript file paths
+ */
+const getEntries = basePath => {
+    let modules = fs.readdirSync(basePath);
+
+    let entryFiles = modules.map((dir) => {
+        let module = path.resolve(basePath, dir);
+        let entry = _.find(fs.readdirSync(module), file => /main\.js/.test(file));
+
+        return path.resolve(basePath, module, entry);
+    });
+
+    return _.keyBy(entryFiles, file => getBundleName(file));
+};
+
+let config = {
+    entry: getEntries(path.resolve(__dirname, 'module')),
     output: {
         filename: '[name].bundle.js'
     }
 };
+
+module.exports = config;
