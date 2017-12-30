e75389ba1d0a8875b84a3d59bda52a34b124cfe4
Eoghan O'Loughlin <eoghanoloughlin@ymail.com>
2017-12-30 21:18:12 +0000

    Tidied up config and generating output from deeper entry points

---
+++ webpack-util.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const fs = require('fs');
+const _ = require('lodash');
+
+module.exports = {
+    /**
+     * A trick to return a webpack bundle name for a deep filePath.
+     * To explain: a webpack bundle whose entry name contains slashes will be written to disk
+     * according to those slashes so you can output to a sub directory of any depth (I think).
+     * Eg: 'public/js/module-one/bundle' will be bundled as 'public/js/module-one/bundle.js'
+     * @param  {String} filePath    The file path for an entry path
+     * @return {String}
+     */
+    getBundleName(filePath) {
+        let parts = filePath.split('/'),
+            newParts = _.dropRight(_.slice(parts, parts.indexOf('public')));
+
+        newParts.push('bundle');
+
+        return newParts.join('/');
+    },
+
+    /**
+     * Returns an array of js files for webpack to process,
+     * where the entry file is named 'main.js'
+     * @param  {String} basePath    The top level directory containing js modules
+     * @return {Array}              An array of javascript file paths
+     */
+    getEntries(basePath) {
+        let modules = fs.readdirSync(basePath);
+
+        let entryFiles = modules.map((dir) => {
+            let module = path.resolve(basePath, dir);
+            let entry = _.find(fs.readdirSync(module), file => /main\.js/.test(file));
+
+            return path.resolve(basePath, module, entry);
+        });
+
+        return _.keyBy(entryFiles, file => this.getBundleName(file));
+    }
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,10 @@
 const path = require('path');
-const fs = require('fs');
-const _ = require('lodash');
-const readDirRecursive = require('recursive-readdir-synchronous');
-
-/**
- * Returns a webpack bundle name for filePath
- * It's a bit of a hack. For a path like: '...module/module1/module1/main.js'
- * it will return 'module/module1/module1'
- * @param  {String} filePath    The file path for an entry path
- * @return {String}
- */
-const getBundleName = filePath => {
-    let parts = filePath.split('/'),
-        dir = parts[parts.length - 3],
-        moduleName = parts[parts.length - 2];
-
-    return [dir, moduleName, moduleName].join('/');
-}
-
-/**
- * Returns an array of js files for webpack to process
- * @param  {String} basePath    The top level directory containing js modules
- * @return {Array}              An array of javascript file paths
- */
-const getEntries = basePath => {
-    let modules = fs.readdirSync(basePath);
-
-    let entryFiles = modules.map((dir) => {
-        let module = path.resolve(basePath, dir);
-        let entry = _.find(fs.readdirSync(module), file => /main\.js/.test(file));
-
-        return path.resolve(basePath, module, entry);
-    });
-
-    return _.keyBy(entryFiles, file => getBundleName(file));
-};
+const util = require('./webpack-util');
 
 let config = {
-    entry: getEntries(path.resolve(__dirname, 'module')),
+    entry: util.getEntries(path.resolve(__dirname, 'public/js')),
     output: {
-        filename: '[name].bundle.js'
+        filename: '[name].js'
     }
 };
 
